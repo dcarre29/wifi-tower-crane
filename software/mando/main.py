9b913e6757faf7cb50427614c511d8252b23426e
dcarre29 <dcarre29@gmail.com>
2023-01-27 15:31:15 +0100

    nueva version

--- software/mando/main.py
+++ software/mando/main.py
@@ -9,7 +9,7 @@ import ADS1x15.adafruit_ads1x15.ads1115 as ADS
 from ADS1x15.adafruit_ads1x15.analog_in import AnalogIn
 import socket
 import sys
-
+from threading import Thread
 orientacion = 0
 distcarro = 0
 distcarga = 0
@@ -18,7 +18,7 @@ x = 0
 y = 0
 z = 0
 serverAdress = ('192.168.1.135', 8080)
-timeout = 0.5
+timeout = 5
 paquetes = 0
 
 url = 'http://192.168.1.134/cam-lo.jpg'
@@ -28,42 +28,46 @@ cv2.namedWindow(winName, cv2.WINDOW_NORMAL)
 
 js = Joystick()
 
+def electronica(): #Verificamos el estado de la electronica del mando
+    global x, y, z
+    while True: 
+        x = js.getAxis("x")
+        y = js.getAxis("y")
+        z = js.getAxis("z")
 
-
-
-while True:
+def comunicacion(): #Enviamos y recibimos las variables
+    global orientacion, distcarga, distcarro, peso, paquetes
+    while True:
     
 
-    try:
-        sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ## Objeto del socket cliente declarado como socket de flujo
-        print('Conecting with: ', serverAdress)
-        sc.connect(serverAdress)
-        sc.settimeout(timeout)
-        print("Conected")
+        try:
+            sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ## Objeto del socket cliente declarado como socket de flujo
+            print('Conecting with: ', serverAdress)
+            sc.connect(serverAdress)
+            sc.settimeout(timeout)
+            print("Conected")
 
-        while True:
-            x = js.getAxis("x")
-            y = js.getAxis("y")
-            z = js.getAxis("z")
+            while True:
 
-            sc.send("ejex".encode('utf-8'))
-            data = sc.recv(1)
-            if data == bytes(1):
-                sc.send(x.to_bytes(1,'big'))
+
+                sc.send("ejex".encode('utf-8'))
                 data = sc.recv(1)
                 if data == bytes(1):
-                    sc.send("ejey".encode('utf-8'))
+                    sc.send(x.to_bytes(1,'big'))
                     data = sc.recv(1)
-                    
                     if data == bytes(1):
-                        sc.send(y.to_bytes(1,'big'))
+                        sc.send("ejey".encode('utf-8'))
                         data = sc.recv(1)
-
+                    
                         if data == bytes(1):
-                            sc.send("ejez".encode('utf-8'))
+                            sc.send(y.to_bytes(1,'big'))
                             data = sc.recv(1)
 
-                            if data == bytes(1): 
+                            if data == bytes(1):
+                                sc.send("ejez".encode('utf-8'))
+                                data = sc.recv(1)
+
+                                if data == bytes(1): 
                                     sc.send( z.to_bytes(1,'big'))
                                     data = sc.recv(1)
                                     #print("enviado")
@@ -94,7 +98,7 @@ while True:
                                                     peso = int.from_bytes(sc.recv(4),'big')
                                                     sc.send(bytes(1))
                                                     paquetes +=1
-                                                    #print("{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}".format(orientacion, distcarro, distcarga, peso , paquetes))
+                                                    print("{:>5}\t{:>5}\t{:>5}".format(x, y ,z ))
                                             
                                             else:
                                                 print("error en distcarga")
@@ -103,29 +107,45 @@ while True:
                                     else: 
                                         print("error en orientacion")
 
-            imgResponse = urllib.request.urlopen(url)
-            imgNp = np.array(bytearray(imgResponse.read()), dtype = np.uint8)
-            img = cv2.imdecode(imgNp, -1)
-
-
-            cv2.setWindowProperty(winName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
-            cv2.putText(img,"Rotacion:", (0,235), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255,1)
-            cv2.putText(img,"Carga:", (0,205), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255,1)
-            cv2.putText(img,str(orientacion), (150,235), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255,1)
-            cv2.putText(img,str(distcarro), (150,205), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255,1)
-            cv2.imshow(winName, img)
-            tecla = cv2.waitKey(1) & 0xFF
-            if tecla == ord('q'):
-                break
-        cv2.destroyAllWindows()
+            
     
-    except KeyboardInterrupt:
-        cv2.destroyAllWindows()
-        sc.close()
-    except socket.timeout: 
-        print("timeOut cliente")
-        sc.close()
+        except KeyboardInterrupt:
+            cv2.destroyAllWindows()
+            sc.close()
+
+        except socket.timeout: 
+            print("timeOut cliente")
+            cv2.destroyAllWindows()
+            sc.close()
 
     
 
+def grafica(): 
 
+    while True: 
+
+        imgResponse = urllib.request.urlopen(url)
+        imgNp = np.array(bytearray(imgResponse.read()), dtype = np.uint8)
+        img = cv2.imdecode(imgNp, -1)
+
+
+        cv2.setWindowProperty(winName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
+        cv2.putText(img,"Rotacion:", (0,235), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255,1)
+        cv2.putText(img,"Distcarro:", (0,205), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255,1)
+        cv2.putText(img,str(orientacion), (150,235), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255,1)
+        cv2.putText(img,str(distcarro), (150,205), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255,1)
+        cv2.imshow(winName, img)
+        tecla = cv2.waitKey(1) & 0xFF
+        if tecla == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+
+
+hilo1 = Thread(target=comunicacion)
+hilo2 = Thread(target=grafica)
+hilo3 = Thread(target=electronica)
+
+hilo1.start()
+hilo2.start()
+hilo3.start()
