953cd080d1fbd9c16f7b977664621709a86fde92
dcarre29 <dcarre29@gmail.com>
2023-01-31 23:55:52 +0100

    version comentada

--- software/mando/joystick10.py
+++ software/mando/joystick10.py
@@ -1,49 +1,66 @@
-import time
 import board
+#se importa para interactuar con los pines de la RPi
 import busio
+#Se importa para interactuar con el bus I2C
 import ADS1x15.adafruit_ads1x15.ads1115 as ADS
+#libreria para la gestion de canales ADC
 from ADS1x15.adafruit_ads1x15.analog_in import AnalogIn
-
+#libreria para el valor analogico del ADC
 
 
 class Joystick():
 
     def __init__(self):
-
+        #metodo constructor
         self.canal = busio.I2C(board.SCL, board.SDA)
+        #declaracion de los pines del bus I2C
         self.adc = ADS.ADS1115(self.canal)
+        #instanciacion del ADC
         self.__x = int(AnalogIn(self.adc, ADS.P2).value)
+        #asignacion del valor del canal 2 del ADC al eje X
         self.__y = int(AnalogIn(self.adc, ADS.P1).value)
+        #asignacion del valor del canal 1 del ADC al eje Y
         self.__z = int(AnalogIn(self.adc, ADS.P0).value)
+        #asignacion del valor del canal 0 del ADC al eje Z
 
 
     def getAxis(self,eje):
+        #metodo para transformar los datos del ADC
         if eje == 'x':
+            #si eje corresponde a X
             self.__x = AnalogIn(self.adc, ADS.P2).value*(255/32768)
+            #lectura del canal 2 y transformacion de 15 bits a 8 bits
+            #para tener un valor de consigna mas estable
             if self.__x < 0: 
                 self.__x = 0
             if self.__x > 255:
                 self.__x = 255
-
+            #Garantizacion de que el eje X estara entre 0 y 255
             return int(self.__x) 
+            #retorna el valor calculado
 
         elif eje == 'y':
-
+            #si eje corresponde a Y
             self.__y = AnalogIn(self.adc, ADS.P1).value*(255/32768)
-
+            #lectura del canal 1 y transformacion de 15 bits a 8 bits
+            #para tener un valor de consigna mas estable
             if self.__y < 0: 
                 self.__y = 0
             if self.__y > 255:
                 self.__y = 255
-
+            #Garantizacion de que el eje y estara entre 0 y 255
             return int(self.__y)
+            #retorna el valor calculado
             
         elif eje == 'z':
-
+            #si eje corresponde a Z
             self.__z = AnalogIn(self.adc, ADS.P0).value*(255/32768)
+            #lectura del canal 0 y transformacion de 15 bits a 8 bits
+            #para tener un valor de consigna mas estable
             if self.__z < 0: 
                 self.__z = 0
             if self.__z > 255:
                 self.__z = 255
-
+            #garantizacion de que el eje z estara entre 0 y 255
             return int(self.__z)
+            #retorna el valor calculado
--- software/mando/main.py
+++ software/mando/main.py
@@ -1,151 +1,186 @@
-from joystick10 import Joystick
+from joystick10 import Joystick 
+#se importa la libreria Joystick para el manejo del joystick
 import cv2 
+#se importa la libreria open-CV para el manejo de las partes gráficas
 import urllib.request 
+#se importa la libreria urllib para enviar peticiones HTTP a una url
 import numpy as np
-import time
-import board
-import busio
-import ADS1x15.adafruit_ads1x15.ads1115 as ADS
-from ADS1x15.adafruit_ads1x15.analog_in import AnalogIn
+#se importa la libreria numpy para trabajar con arrays
 import socket
-import sys
+#se importa la libreria socket para la comunicacion mando grua
 from threading import Thread
+#se importa la libreria thread para el manejo de los hilos
+import RPi.GPIO as GPIO
+#libreria para el manejo del los pines de la RPi
+
+
 orientacion = 0
 distcarro = 0
 distcarga = 0
 peso = 0
-x = 0
-y = 0
-z = 0
-serverAdress = ('192.168.1.135', 8080)
-timeout = 5
-paquetes = 0
+#valores iniciales de los parametros de posicionamiento de la grua
+x = 130
+y = 130
+z = 130
+#valores iniciales de las consignas del mando
+#correspondientes a motores frenados
 
-url = 'http://192.168.1.134/cam-lo.jpg'
+serverAdress = ('192.168.1.136', 8080)
+#direccion del servidor IP y puerto
+timeout =0.8
+#tiempo limite de bloqueo en el metodo recv
+                                                                                                                        #borrar al finalizar
 
+#variables de la parte grafica
+url = 'http://192.168.1.138/cam-hi.jpg'
+#direccion url de la camara
 winName = 'Mi camara'
+#nombre de la ventana que abriremos con opencv
+font = cv2.FONT_HERSHEY_SIMPLEX
+#tipo de letra
+color=(0, 233,255)
+#color de la letra en RGB
+fontScale = 1
+#tamaño de letra
+thickness = 2
+#grosor de la letra
 cv2.namedWindow(winName, cv2.WINDOW_NORMAL)
-
+#creacion de la ventana para el LCD
 js = Joystick()
+#instanciacion de la clase joystick
 
-def electronica(): #Verificamos el estado de la electronica del mando
+b_accion = 18
+#numero de pin del boton accion
+GPIO.setup(b_accion, GPIO.IN, pull_up_down= GPIO.PUD_DOWN)
+#configuracion del boton de accion
+#configuracion pull-down
+
+
+def electronica(): 
+    #Verificamos el estado de la electronica del mando
     global x, y, z
-    while True: 
-        x = js.getAxis("x")
-        y = js.getAxis("y")
-        z = js.getAxis("z")
+    #variables globales que se manipulan en este metodo
+    #en este metodo se manipulan las consignas del joystick
 
-def comunicacion(): #Enviamos y recibimos las variables
-    global orientacion, distcarga, distcarro, peso, paquetes
     while True:
-    
+                                                                                                                                            #implementar boton de hombre muerto        
+        if(GPIO.input(b_accion) == True):
+            #si el boton de accion está pulsado
+            x = js.getAxis("x")
+            y = js.getAxis("y")
+            z = js.getAxis("z")
+        #obtencion de los valores del ADC para cada eje transformados
+        #entre 0 y 255.
+        else:
+        #si no esta pulsado
+            x = y = z = 120
+        #enviamos 120 para frenar motores
 
+        
+       
+def comunicacion(): 
+    #Enviamos y recibimos las variables
+    global orientacion, distcarga, distcarro, peso, paquetes
+    #variables globales que se manipulan en este metodo
+
+    while True:
         try:
-            sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ## Objeto del socket cliente declarado como socket de flujo
+            sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+            #Objeto del socket cliente declarado como socket de flujo
             print('Conecting with: ', serverAdress)
+            #impresion del estado por consola
             sc.connect(serverAdress)
+            #se envia peticion de conexion al servidor
+            #permanece bloqueado hasta recibir confirmacion
             sc.settimeout(timeout)
+            #se establece el tiempo maximo de bloqueo en el metodo recv
             print("Conected")
+            #impresion del estado por consola
 
             while True:
+                sc.send( x.to_bytes(1,'big'))
+                #envio de la consigna del eje x en bytes
+                orientacion = int.from_bytes(sc.recv(15),'big')
+                #recepcion de la variable orientacion de la grua
+                sc.send( y.to_bytes(1,'big'))
+                #envio de la consigna del eje y en bytes
+                distcarro = int.from_bytes(sc.recv(15),'big')
+                #recepcion de la variable distcarro de la grua
+                sc.send( z.to_bytes(1,'big'))
+                #envio de la consigna del eje z en bytes
+                distcarga = int.from_bytes(sc.recv(15),'big')
+                #recepcion de la variable distcarga de la grua
+                sc.send("peso".encode('utf-8'))
+                #envio de la orden peso a la grua
+                peso = int.from_bytes(sc.recv(15),'big')
+                #recepcion del peso transportado por la grua
 
-
-                sc.send("ejex".encode('utf-8'))
-                data = sc.recv(1)
-                if data == bytes(1):
-                    sc.send(x.to_bytes(1,'big'))
-                    data = sc.recv(1)
-                    if data == bytes(1):
-                        sc.send("ejey".encode('utf-8'))
-                        data = sc.recv(1)
-                    
-                        if data == bytes(1):
-                            sc.send(y.to_bytes(1,'big'))
-                            data = sc.recv(1)
-
-                            if data == bytes(1):
-                                sc.send("ejez".encode('utf-8'))
-                                data = sc.recv(1)
-
-                                if data == bytes(1): 
-                                    sc.send( z.to_bytes(1,'big'))
-                                    data = sc.recv(1)
-                                    #print("enviado")
-                                    
-                                    data = sc.recv(11).decode('utf-8')
-
-                                    if data == 'orientacion':
-                                        sc.send(bytes(1))
-                                        orientacion = int.from_bytes(sc.recv(4),'big')
-                                        sc.send(bytes(1))
-                                        data = sc.recv(9).decode('utf-8')
-
-                                        if data == "distcarro":
-                                            sc.send(bytes(1))
-                                            distcarro = int.from_bytes(sc.recv(4),'big')
-                                            sc.send(bytes(1))
-                                            data = sc.recv(9).decode('utf-8')
-
-                                            if data == "distcarga": 
-                                                sc.send(bytes(1))
-                                                distcarga = int.from_bytes(sc.recv(4),'big')
-                                                sc.send(bytes(1))
-                                                data = sc.recv(4).decode('utf-8')
-
-                                                if data == "peso":
-
-                                                    sc.send(bytes(1))
-                                                    peso = int.from_bytes(sc.recv(4),'big')
-                                                    sc.send(bytes(1))
-                                                    paquetes +=1
-                                                    print("{:>5}\t{:>5}\t{:>5}".format(x, y ,z ))
-                                            
-                                            else:
-                                                print("error en distcarga")
-                                        else:
-                                            print("error en distcarro")
-                                    else: 
-                                        print("error en orientacion")
-
-            
-    
+                
         except KeyboardInterrupt:
             cv2.destroyAllWindows()
             sc.close()
 
-        except socket.timeout: 
+        except socket.timeout:
+            #tratamiento de la excepcion timeout
             print("timeOut cliente")
+            #impresion del estado por consola
             cv2.destroyAllWindows()
+            #destruccion de la ventana grafica
             sc.close()
-
+            #cierre del socket
+            
     
 
 def grafica(): 
-
+    #metodo que se encarga de la parte grafica del mando
     while True: 
+        try:
+            while True:
+                imgResponse = urllib.request.urlopen(url,timeout=5)
+                #se abre el URL declarado anteriormente
+                
+                imgNp = np.array(bytearray(imgResponse.read()), dtype = np.uint8)
+                #se transforman los datos recibidos en un array de bytes
+                
+                img = cv2.imdecode(imgNp, -1)
+                #se tranforman los arrays en un streaming de imagenes
+                cv2.setWindowProperty(winName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+                #configuramos la pantalla como pantalla completa
+                cv2.putText(img,"Orientacion:", (0,430), cv2.FONT_HERSHEY_SIMPLEX, fontScale ,color, thickness)
+                cv2.putText(img,"Distcarro:", (0,460), cv2.FONT_HERSHEY_SIMPLEX, fontScale ,color, thickness)
+                cv2.putText(img,str(orientacion), (210,430), cv2.FONT_HERSHEY_SIMPLEX, fontScale ,color, thickness)
+                cv2.putText(img,"mm", (270,430), cv2.FONT_HERSHEY_SIMPLEX, fontScale ,color, thickness)
+                cv2.putText(img,str(distcarro), (210,460), cv2.FONT_HERSHEY_SIMPLEX, fontScale ,color, thickness)
+                cv2.putText(img,"mm", (270,460), cv2.FONT_HERSHEY_SIMPLEX, fontScale ,color, thickness)
+                cv2.putText(img,"DistCarga:", (340,460), cv2.FONT_HERSHEY_SIMPLEX, fontScale ,color, thickness)
+                cv2.putText(img,"Peso:", (340,430), cv2.FONT_HERSHEY_SIMPLEX, fontScale ,color, thickness)
+                cv2.putText(img,str(distcarga), (500,460), cv2.FONT_HERSHEY_SIMPLEX, fontScale ,color, thickness)
+                cv2.putText(img,"mm", (570,460), cv2.FONT_HERSHEY_SIMPLEX, fontScale ,color, thickness)
+                cv2.putText(img,str(peso), (500,430), cv2.FONT_HERSHEY_SIMPLEX, fontScale ,color, thickness)
+                cv2.putText(img,"g", (570,430), cv2.FONT_HERSHEY_SIMPLEX, fontScale ,color, thickness)
+                #se superponen las variables de posicionamiento de la grua en el streaming
+                cv2.imshow(winName, img)
+                #se refresca la imagen
 
-        imgResponse = urllib.request.urlopen(url)
-        imgNp = np.array(bytearray(imgResponse.read()), dtype = np.uint8)
-        img = cv2.imdecode(imgNp, -1)
+                tecla = cv2.waitKey(1) & 0xFF
+                if tecla == ord('q'):
+                    break
+                #atajo de teclado para cerrar la ventana
+
+        except ConnectionResetError:
+            continue
+            #si se resetea la conxion se vuelve a conectar
+        cv2.destroyAllWindows()
+        #si el hilo termina se cierra la ventana
 
 
-        cv2.setWindowProperty(winName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
-        cv2.putText(img,"Rotacion:", (0,235), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255,1)
-        cv2.putText(img,"Distcarro:", (0,205), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255,1)
-        cv2.putText(img,str(orientacion), (150,235), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255,1)
-        cv2.putText(img,str(distcarro), (150,205), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255,1)
-        cv2.imshow(winName, img)
-        tecla = cv2.waitKey(1) & 0xFF
-        if tecla == ord('q'):
-            break
-    cv2.destroyAllWindows()
-
-
-
-hilo1 = Thread(target=comunicacion)
-hilo2 = Thread(target=grafica)
-hilo3 = Thread(target=electronica)
+hilo1 = Thread(target=comunicacion, name=comunicacion)
+hilo2 = Thread(target=grafica, name= grafica)
+hilo3 = Thread(target=electronica, name= electronica)
+#se declaran los hilos y se asocian al metodo correspondiente
 
 hilo1.start()
 hilo2.start()
 hilo3.start()
+
+#se lanzan los tres hilos
